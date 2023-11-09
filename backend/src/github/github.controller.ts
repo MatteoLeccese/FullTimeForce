d10570b10c30163d955efa98a766a7022ae157a2
matteo leccese <matteoleccese2099@gmail.com>
2023-11-09 17:02:50 -0400

    A 'github' module was created which encapsulates a service that is responsible for retrieving information from the GitHub API, a controller which has an endpoint that uses the GitHub service to return the commit list of a project or an error if the data provided was incorrect.

--- backend/src/github/github.controller.ts
+++ backend/src/github/github.controller.ts
@@ -1,7 +1,41 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  HttpException,
+  HttpStatus,
+  Query,
+} from '@nestjs/common';
 import { GithubService } from './github.service';
+import { firstValueFrom } from 'rxjs';
+import { CommitHistory } from './interfaces/github.interfaces';
+import { AxiosResponse } from 'axios';
 
 @Controller('github')
 export class GithubController {
   constructor(private readonly githubService: GithubService) {}
+
+  @Get()
+  async getAllCommits(
+    @Query('user') user: string,
+    @Query('repository') repository: string,
+  ): Promise<CommitHistory[]> {
+    try {
+      const { data } = (await firstValueFrom(
+        this.githubService.getAllCommits(user, repository),
+      )) as AxiosResponse<CommitHistory[]>;
+      return data;
+    } catch (error: unknown) {
+      throw new HttpException(
+        {
+          status: HttpStatus.NOT_FOUND,
+          error:
+            'The project you are tring to fetch was not found. Make sure the user and the repository are correct.',
+        },
+        HttpStatus.NOT_FOUND,
+        {
+          cause: error,
+        },
+      );
+    }
+  }
 }
--- backend/src/github/github.service.ts
+++ backend/src/github/github.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { HttpService } from '@nestjs/axios';
+import { AxiosResponse } from 'axios';
 
 @Injectable()
-export class GithubService {}
+export class GithubService {
+  constructor(private readonly httpService: HttpService) {}
+
+  getAllCommits(
+    user: string,
+    repository: string,
+  ): Observable<AxiosResponse<any>> {
+    return this.httpService.get(
+      `https://api.github.com/repos/${user}/${repository}/commits`,
+    );
+  }
+}
